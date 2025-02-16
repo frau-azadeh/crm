abb1dcb7c23f57001aff39e2b3deb4a9e8747b1d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-16 02:46:06 -0800

    add link

--- src/app/components/layout/Sidebar.tsx
+++ src/app/components/layout/Sidebar.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 const Sidebar = () => {
   return (
     <div className="bg-secondary text-white w-60 min-h-screen p-4 hidden md:block">
       <ul className="space-y-4">
-        <li className="cursor-pointer hover:text-accent">داشبورد</li>
-        <li className="cursor-pointer hover:text-accent">مشتریان</li>
-        <li className="cursor-pointer hover:text-accent">تنظیمات</li>
+        <li className="cursor-pointer hover:text-accent">
+            <Link href="/">داشبورد</Link>
+        </li>
+        <li className="cursor-pointer hover:text-accent">
+            <Link href="/customers">مشتریان</Link>
+        </li>
+        <li className="cursor-pointer hover:text-accent">
+            <Link href="#">تنظیمات</Link>
+        </li>
       </ul>
     </div>
   );
