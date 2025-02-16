596718802ecd60df84334344216ac6abc2b194a7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-16 02:37:36 -0800

    add

---
+++ src/hooks/useCustomers.ts
@@ -0,0 +1,11 @@
+import { getCustomers } from "@/services/customerService";
+import { useQuery } from "@tanstack/react-query";
+import { Customer } from "@/types/customer";
+
+export function useCustomers () {
+    return useQuery<Customer[]>({
+        queryKey:["customers"],
+        queryFn: getCustomers,
+        staleTime: 1000*60*5,
+    })
+}
