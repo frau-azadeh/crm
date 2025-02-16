62a94e0666c553dd493be0820c61873041538f39
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-16 00:53:58 -0800

    add var color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#2563EB', // آبی اصلی
+        secondary: '#1E293B', // طوسی تیره
+        accent: '#FACC15', // زرد تاکیدی
+        background: '#F8FAFC', // پس‌زمینه روشن
+        textPrimary: '#1E293B', // متن اصلی
+        textSecondary: '#64748B', // متن فرعی
       },
     },
   },
