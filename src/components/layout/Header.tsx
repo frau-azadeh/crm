6572e04764a0b3ee9cda6b01e4481211169f6fbd
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-16 09:54:31 -0800

    refactor

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import { Menu } from "lucide-react";
+
+const Header = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
+  return (
+    <div className="bg-primary text-white shadow-md p-4 justify-between items-center flex">
+      <h1 className="text-lg font-bold">مدیریت مشتریان</h1>
+      <button className="block md:hidden" onClick={toggleSidebar}>
+        <Menu className="w-6 h-6" />
+      </button>
+    </div>
+  );
+};
+
+export default Header;
