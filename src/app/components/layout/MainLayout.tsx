b50b9405c4fc82e0fdc5ee5d936aa4ae968d3337
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-16 02:37:21 -0800

    refactor

--- src/app/components/layout/MainLayout.tsx
+++ src/app/components/layout/MainLayout.tsx
@@ -1,14 +1,13 @@
-import { ReactNode } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
-const MainLayout = ({ children }: { children: ReactNode }) => {
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className="flex flex-col h-screen">
       <Header />
-      <div>
+      <div className="flex flex-1">
         <Sidebar />
-        <main>{children}</main>
+        <main className="flex-1 bg-background p-6 overflow-auto">{children}</main>
       </div>
     </div>
   );
