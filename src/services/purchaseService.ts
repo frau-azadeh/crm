2546d1b4dd8eb96e384c496a1c7bc363c15fa3dd
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-17 00:33:56 -0800

    update

--- src/services/purchaseService.ts
+++ src/services/purchaseService.ts
@@ -1,16 +1,18 @@
 import axios from "axios";
-import { Purchase } from "@/types/purchase";
+import { Purchase, PurchaseInput } from "@/types/purchase";
 
 const API_URL = "https://67b1b1393fc4eef538ea6972.mockapi.io/purchase";
 
-export async function getPurchasesByCustomerId(customerId: number) {
-  const response = await axios.get<Purchase[]>(
-    `${API_URL}?customerId=${customerId}`,
-  );
-  return response.data;
-}
+export const addPurchase = async (purchase: PurchaseInput): Promise<Purchase> => {
+  const res = await axios.post<Purchase>(API_URL, purchase);
+  return res.data;
+};
 
-export async function addPurchase(purchase: Omit<Purchase, "id">) {
-  const response = await axios.post(API_URL, purchase);
-  return response.data;
-}
+export const getCustomerPurchases = async (customerId: number): Promise<Purchase[]> => {
+  const res = await axios.get<Purchase[]>(`${API_URL}?customerId=${customerId}`);
+  return res.data.map((p) => ({
+    ...p,
+    customerId: Number(p.customerId),
+    amount: Number(p.amount),
+  }));
+};
