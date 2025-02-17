def21e97ef0d2a99100a88263f85ef24e1ccdaf7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-17 00:33:11 -0800

    update

---
+++ src/components/dashboard/dashboardContent.tsx
@@ -0,0 +1,28 @@
+import { TopCustomer } from "@/types/dashboard";
+import { StatisticsCard } from "./StatisticsCard";
+import { TopCustomersList } from "./TopCustomersList";
+import SalesChart from "./SalesChart";
+
+interface DashboardContentProps {
+  totalCustomers: number;
+  totalPurchases: number;
+  topCustomers: TopCustomer[];
+}
+
+export function DashboardContent({
+  totalCustomers,
+  totalPurchases,
+  topCustomers,
+}: DashboardContentProps) {
+  return (
+    <div className="p-6 space-y-6">
+      <h1 className="text-2xl font-bold text-gray-800">داشبورد فروشگاه</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <StatisticsCard title="تعداد کل مشتریان" value={totalCustomers} />
+        <StatisticsCard title="تعداد کل خریدها" value={totalPurchases} />
+      </div>
+      <TopCustomersList customers={topCustomers} />
+      <SalesChart/>
+    </div>
+  );
+}
