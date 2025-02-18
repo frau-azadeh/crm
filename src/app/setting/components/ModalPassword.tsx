c905b31dbb9f6439b3e5875dc4f3be6f11cd26f5
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-18 01:37:31 -0800

    update prettier

--- src/app/setting/components/ModalPassword.tsx
+++ src/app/setting/components/ModalPassword.tsx
@@ -1,22 +1,28 @@
 import { ReactNode } from "react";
 
 type ModalPasswordProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
 
-    isOpen: boolean;
-    onClose: () => void;
-    children: ReactNode;
-}
-
-export default function ModalPassword ({isOpen, onClose, children}: ModalPasswordProps) {
-    if (! isOpen ) return null;
-    return(
-        <div className="fixed inset-0 bg-black opacity-50 flex items-center justify-center">
-            <div className="bg-white p-6 max-w-md w-full rounded-lg shadow-lg relative">
-                <button onClick={onClose} className="top-2 right-4 text-gray-950 absolute font-bold text-xl">
-                    ×
-                </button>
-                {children}
-            </div>
-        </div>
-    )
+export default function ModalPassword({
+  isOpen,
+  onClose,
+  children,
+}: ModalPasswordProps) {
+  if (!isOpen) return null;
+  return (
+    <div className="fixed inset-0 bg-black opacity-50 flex items-center justify-center">
+      <div className="bg-white p-6 max-w-md w-full rounded-lg shadow-lg relative">
+        <button
+          onClick={onClose}
+          className="top-2 right-4 text-gray-950 absolute font-bold text-xl"
+        >
+          ×
+        </button>
+        {children}
+      </div>
+    </div>
+  );
 }
--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 import ModalPassword from "./components/ModalPassword";
 
 export default function Setting() {
