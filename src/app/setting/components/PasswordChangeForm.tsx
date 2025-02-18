a47ec5a7642d2e59a3fdee8a3d1f5f4bffe8f3be
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-18 08:24:33 -0800

    update

--- src/app/setting/components/PasswordChangeForm.tsx
+++ src/app/setting/components/PasswordChangeForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 type PasswordFormData = {
   newPassword: string;
@@ -8,40 +8,41 @@ type PasswordFormData = {
 type PasswordFormDataProps = {
   onSubmit: (data: PasswordFormData) => void;
 }
+export default function PasswordChangeForm({onSubmit}:PasswordFormDataProps){
 
-export default function PasswordChangeForm ({onSubmit}:PasswordFormDataProps){
-  const[newPassword, setNewPassword] = useState("");
-  const[confirmPassword, setConfirmPassword] = useState("");
-
-  const handelPassword = (e: React.FormEvent) =>{
-    e.preventDefault();
-    onSubmit({newPassword, confirmPassword});
-  }
+  const {register, handleSubmit, formState: {errors}} = useForm<PasswordFormData>();
+  
   return(
-    <form onSubmit={handelPassword} className="flex flex-col gap-2">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-10">
       <div>
         <label className="block mb-1">پسورد جدید</label>
-        <input
-         type="password"
-         required
-         className="bg-slate-100 border-slate-300 rounded p-2 w-full"
-         onChange={(e)=>setNewPassword(e.target.value)}
+        <input 
+          type="password" 
+          {...register('newPassword', {required:'پسورد جدید الزامی میباشد'})}
+          className="border-2 border-gray-400 bg-gray-50 rounded p-2 w-full focus:outline-none focus:border-blue-500"
         />
+        {errors.newPassword && (
+          <p className="text-red-500 text-sm mt-1">{errors.newPassword.message}</p>
+        )}
       </div>
       <div>
-        <label className="block mb-1">تایید پسورد</label>
+        <label className="block mb-1">تغییر پسورد</label>
         <input 
-          type="password"
-          required
-          className="bg-slate-100 border-slate-300 rounded p-2 w-full"
-          onChange={(e) =>setConfirmPassword(e.target.value)}/>
+          {...register('confirmPassword', {required: 'پر کردن این فیلد الزامی است'})}
+          type="password" 
+          className="border-2 border-gray-400 bg-gray-50 rounded p-2 w-full focus:outline-none focus:border-blue-500"
+
+        />
+        {errors.confirmPassword&&(
+          <p className="text-red-500 text-sm mt-1">{errors.confirmPassword.message}</p>
+        )}
       </div>
       <button 
         type="submit"
-        className="bg-blue-500 hover:bg-blue-900 rounded p-2 text-white  w-full"
+        className=" bg-blue-500 w-full text-white p-2 rounded transition hover:bg-blue-950"
       >
-        تایید
-      </button>
+        تغییر پسورد
+     </button>
     </form>
   )
 }
