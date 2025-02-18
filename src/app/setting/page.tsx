fdbe0c04ec254ed7489f74621b1954ebf2b6124d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-18 08:24:10 -0800

    update

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,38 +1,34 @@
-"use client";
+"use client"
 
-import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import Modal from "./components/ModalPassword";
 import PasswordChangeButton from "./components/PasswordChangeButton";
 import PasswordChangeForm from "./components/PasswordChangeForm";
+import { useState } from "react";
 
 export default function SettingPage() {
-  const [isModalOpen, setModalOpen] = useState(false);
 
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  };
+  const[isModalOpen, setModalOpen] = useState (false);
 
-  const handleCloseModal = () => {
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () =>setModalOpen(false);
+
+  const handlePasswordChangeSubmit = async (data:{newPassword: string; confirmPassword: string}) =>{
+
+    toast.success('پسورد با موفقیت تغییر کرد')
     setModalOpen(false);
-  };
 
-  const handlePasswordChangeSubmit = async (data: {
-    newPassword: string;
-    confirmPassword: string;
-  }) => {
-    setModalOpen(false);
-  };
-
-  return (
-    <div className="flex ">
-      <div className="bg-white rounded-lg p-10 shadow-md">
-        <PasswordChangeButton onClick={handleOpenModal} />
+  }
+  return(
+    <div className="flex">
+      <div className="w-full max-w-md rounded-lg shadow bg-white p-6">
+        <PasswordChangeButton onClick={handleOpenModal}/>
       </div>
-
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-        <h2 className="text-xl font-bold mb-4">تغییر پسورد</h2>
-        <PasswordChangeForm onSubmit={handlePasswordChangeSubmit} />
+        <span className="text-xl font-bold mb-4">فرم تغییر پسورد</span>
+        <PasswordChangeForm onSubmit={handlePasswordChangeSubmit}/>
       </Modal>
+      <Toaster position="top-center"/>
     </div>
-  );
+  )
 }
