b588c0e753217ded05ac5fd71e3eae90fda985e6
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-18 04:08:15 -0800

    update

--- src/app/setting/components/PasswordChangeButton.tsx
+++ src/app/setting/components/PasswordChangeButton.tsx
@@ -1,13 +1,13 @@
 type PasswordChangeButtonProps = {
-  onClick: () => void;
-};
-
-export default function PasswordChangeButton({
-  onClick,
-}: PasswordChangeButtonProps) {
-  return (
-    <button className="text-blue-600 " onClick={onClick}>
-      تغییر پسورد
-    </button>
-  );
+    onClick: () => void;
+}
+export default function PasswordChangeButton ({onClick}: PasswordChangeButtonProps) {
+    return(
+        <button  
+            className="text-blue-500 font-bold text-lg hover:text-blue-950 transition"
+            onClick={onClick}
+        >
+            تغییر پسورد
+        </button>
+    )
 }
