f5120df21a9edd28a66ae891e6484a2944061aed
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-19 04:21:13 -0800

    cheack user login or not

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,13 +1,34 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import MainLayout from "@/components/layout/MainLayout";
 import QueryProvider from "@/providers/QueryProvider";
 import { Toaster } from "react-hot-toast";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const router = useRouter();
+
   const isAuthPage = pathname === "/login";
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token && !isAuthPage) {
+      router.push("/login");
+    } else if (token && isAuthPage) {
+      router.push("/");
+    } else {
+      setIsCheckingAuth(false);
+    }
+  }, [pathname, router, isAuthPage]);
+
+  // تا وقتی که وضعیت احراز هویت چک نشده، هیچی نشون نده
+  if (isCheckingAuth) {
+    return null; // یا می‌تونی لودینگ بگذاری
+  }
 
   return (
     <QueryProvider>
