62b7d5ffc17035b90ed8e77734e329c19dd5df64
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-19 11:31:59 -0800

    update

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,10 +1,10 @@
-import axiosInstance from "@/lib/axiosInstance";
+import { authApi } from "@/lib/axiosInstance"
 import { User } from "@/types/user";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const { fullName, password } = await request.json();
-  const { data: users } = await axiosInstance.get<User[]>("/login");
+  const { data: users } = await authApi.get<User[]>("/login");
 
   const user = users.find(
     (u) => u.fullName === fullName && u.password === password,
