813340185d4f3585e69166869effcde1bf953a2f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-19 04:12:28 -0800

    update

--- src/app/login/components/Button.tsx
+++ src/app/login/components/Button.tsx
@@ -1,15 +1,16 @@
+import { ReactNode } from "react"
+
 type ButtonProps = {
-    children: React.ReactNode;
-  } & React.ButtonHTMLAttributes<HTMLButtonElement>;
-  
-  export default function Button({ children, ...rest }: ButtonProps) {
-    return (
-      <button
-        {...rest}
-        className="bg-blue-500 text-white w-full py-2 rounded hover:bg-blue-600"
-      >
-        {children}
-      </button>
-    );
-  }
-  
+  children: ReactNode;
+}& React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function Button ({ children, ...rest }: ButtonProps){
+  return(
+    <button
+      {...rest}
+      className="bg-blue-500 text-white hover:bg-blue-900 p-2 rounded shadow-md w-full "
+    >
+      {children}
+    </button>
+  )
+}
