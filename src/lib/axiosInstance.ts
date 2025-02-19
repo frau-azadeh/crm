27b56cdfac2219e7868000c4039ee519767cc629
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-19 02:19:45 -0800

    refactor

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://67b59f1807ba6e59083dafc9.mockapi.io/', // بیس آدرس
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+  baseURL: "https://67b59f1807ba6e59083dafc9.mockapi.io/",
+  headers:{
+    "Content-Type": "application/json",
+  }
+})
 
 export default axiosInstance;
