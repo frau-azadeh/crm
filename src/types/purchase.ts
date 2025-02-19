6ee094a6966eb908ffe7d77294d59428f95bb9af
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-19 11:34:18 -0800

    update

--- src/types/purchase.ts
+++ src/types/purchase.ts
@@ -1,8 +1,13 @@
 export interface Purchase {
-  id: number;
-  customerId: number;
+  id: string;
+  customerId: string;
+  amount: number;
+  date: string;
+  createdAt?: string;
+}
+
+export interface PurchaseInput {
+  customerId: string;
   amount: number;
   date: string;
 }
-
-export type PurchaseInput = Omit<Purchase, "id">;
