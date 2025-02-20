ee0ffe7c6bdc31c7058d80f03548da8d6b0c17e9
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-20 04:33:00 -0800

    update prettier

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -49,7 +49,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray100">
+    <div className="h-screen flex items-center justify-center bg-gray-100">
       <LoginForm onSubmit={onSubmit} />
     </div>
   );
