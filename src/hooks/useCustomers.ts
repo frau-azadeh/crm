a88dc2c4c5649420150563c6ec4332873891b29c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-20 04:06:27 -0800

    delet dont use

--- src/hooks/useCustomers.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCustomers } from "@/services/customerService";
-
-export const useCustomers = () => {
-  return useQuery({
-    queryKey: ["customers"],
-    queryFn: getCustomers,
-  });
-};
