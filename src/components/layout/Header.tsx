b9492087947e72e26c39494f340f14314ba1463b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-20 04:22:41 -0800

    change color

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import { Menu } from "lucide-react";
 
 const Header = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
   return (
-    <div className="bg-primary text-white shadow-md p-4 justify-between items-center flex">
+    <div className="bg-secondary/90 shadow-2xl text-white p-4 justify-between items-center flex ">
       <h1 className="text-lg font-bold">مدیریت مشتریان</h1>
       <button className="block md:hidden" onClick={toggleSidebar}>
         <Menu className="w-6 h-6" />
